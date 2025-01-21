2c19e6d3f46596473822eb771cec77c58afdfbed
Peter Wu <petey888@gmail.com>
2025-01-21 11:38:32 -0800

    Initial Commit

---
+++ recipes/recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipesApp.swift
+//  recipes
+//
+//  Created by Peter Wu on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ recipesTests/recipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  recipesTests.swift
+//  recipesTests
+//
+//  Created by Peter Wu on 1/21/25.
+//
+
+import Testing
+@testable import recipes
+
+struct recipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
