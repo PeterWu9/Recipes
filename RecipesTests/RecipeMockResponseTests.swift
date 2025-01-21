97d184cc48d820b140aff9ead17b9e7246e1ca7f
Peter Wu <petey888@gmail.com>
2025-01-21 14:53:39 -0800

    Implements decode mock testing - test passed

--- RecipesTests/RecipeMockResponseTests.swift
+++ RecipesTests/RecipeMockResponseTests.swift
@@ -4,19 +4,36 @@
 //
 //  Created by Peter Wu on 1/21/25.
 //
-
+import Foundation
 import Testing
+@testable import Recipes
 
 struct RecipeMockResponseTests {
-
+    enum RecipeMockResponseTestsError: Error {
+        case unableToLocateResource(name: String, extension: String)
+    }
+    
     
     /// Tests decoding mocks
     @Test func decodeMock() async throws {
         // Locate resource from bundle
+        let fileName = "RecipesResponseAllRecipes"
+        let fileExtension = ".json"
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: fileExtension) else {
+            throw RecipeMockResponseTestsError
+                .unableToLocateResource(
+                    name: fileName,
+                    extension: fileExtension
+                )
+        }
         // Fetch data from resource
+        let data = try Data(contentsOf: url)
         // Deserialize data and decode into Recipes Response DTO
+        let decoder = JSONDecoder()
+        let response = try decoder.decode(RecipesResponseDTO.self, from: data)
         // Asserts on recipes response dto from json file
-        
+        let firstRecipe = try #require(response.recipes.first)
+        #expect(firstRecipe.cuisine == "Malaysian")
     }
 
 }
