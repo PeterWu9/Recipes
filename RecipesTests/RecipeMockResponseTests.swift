09336c058cc3a71ed9e6d7d46524d1ecf6e64a55
Peter Wu <petey888@gmail.com>
2025-01-22 12:15:28 -0800

    Extracts business logic on decoding data from file in Bundle to its own utility for later reuse

--- RecipesTests/RecipeMockResponseTests.swift
+++ RecipesTests/RecipeMockResponseTests.swift
@@ -17,20 +17,7 @@ struct RecipeMockResponseTests {
     /// Tests decoding mocks
     @Test func decodeMock() async throws {
         // Locate resource from bundle
-        let fileName = "RecipesResponseAllRecipes"
-        let fileExtension = ".json"
-        guard let url = Bundle.main.url(forResource: fileName, withExtension: fileExtension) else {
-            throw RecipeMockResponseTestsError
-                .unableToLocateResource(
-                    name: fileName,
-                    extension: fileExtension
-                )
-        }
-        // Fetch data from resource
-        let data = try Data(contentsOf: url)
-        // Deserialize data and decode into Recipes Response DTO
-        let decoder = JSONDecoder()
-        let response = try decoder.decode(RecipesResponseDTO.self, from: data)
+        let response = try Bundle.main.decode(type: RecipesResponseDTO.self, fileName: "RecipesResponseAllRecipes", fileExtension: ".json")
         // Asserts on recipes response dto from json file
         let firstRecipe = try #require(response.recipes.first)
         #expect(firstRecipe.cuisine == "Malaysian")
