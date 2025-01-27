0a5377653863e519d4bd1db1f82c1f2470728ff8
Peter Wu <petey888@gmail.com>
2025-01-27 12:58:27 -0800

    Implements inital skeleton for view model

--- Recipes/ViewModel/ViewModel.swift
+++ Recipes/ViewModel/ViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import RecipesRepository
+
+@MainActor
+@Observable
+final class ViewModel {
+    var allRecipes = [Recipe]()
+    
+    private let repository: any RecipesRepository
+    
+    init(repository: any RecipesRepository) {
+        self.repository = repository
+    }
+    
+    func fetchAllRecipes() async {
+        do {
+            allRecipes = try await repository.fetchAllRecipes()
+        } catch {
+            // TODO: Error handling
+            print(error.localizedDescription)
+        }
+    }
+}
