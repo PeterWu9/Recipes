b4a5224df19813225aee3e0c7ebe373c7500f84e
Peter Wu <petey888@gmail.com>
2025-01-28 09:27:25 -0800

    Implements view model test on asserting loading state for full load

--- RecipesTests/ViewModelTests.swift
+++ RecipesTests/ViewModelTests.swift
@@ -5,12 +5,27 @@
 //  Created by Peter Wu on 1/28/25.
 //
 
+@testable import Recipes
+@testable import RecipesRepository
 import Testing
 
 struct ViewModelTests {
-
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    
+    @Test func loadingStateFullLoad() async throws {
+        let repository = RemoteRecipesRepository.allRecipes
+        let vm = await ViewModel(repository: repository)
+        await vm.fetchAllRecipes()
+        let fetchedFromRepository = try await repository.fetchAllRecipes().map {
+            RecipeCell
+            .CellData.init(
+                id: $0.id,
+                name: $0.name,
+                cuisineName: $0.cuisine.title,
+                imageUrl: $0.photoUrl?.urlString
+            )}
+        await #expect(
+            vm.loadingState == .loaded(.withLoad(fetchedFromRepository))
+        )
     }
 
 }
