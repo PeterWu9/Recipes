e726eaa9cfad8dce19b08627c1e51a1badf98d8d
Peter Wu <petey888@gmail.com>
2025-01-28 09:33:52 -0800

    Implements empty and error loading state tests

--- RecipesTests/ViewModelTests.swift
+++ RecipesTests/ViewModelTests.swift
@@ -27,5 +27,25 @@ struct ViewModelTests {
             vm.loadingState == .loaded(.withLoad(fetchedFromRepository))
         )
     }
+    
+    @Test func loadingStateEmpty() async throws {
+        let vm = await ViewModel(repository: RemoteRecipesRepository.empty)
+        await vm.fetchAllRecipes()
+        await #expect(vm.loadingState == .loaded(.empty))
+    }
+    
+    @Test func loadingStateWithError() async throws {
+        let repository = RemoteRecipesRepository.malFormed
+        let vm = await ViewModel(repository: RemoteRecipesRepository.malFormed)
+        await vm.fetchAllRecipes()
+        do {
+            _ = try await repository.fetchAllRecipes()
+        } catch {
+            await #expect(
+                vm.loadingState ==
+                    .loaded(.withError(error.localizedDescription))
+            )
+        }
+    }
 
 }
