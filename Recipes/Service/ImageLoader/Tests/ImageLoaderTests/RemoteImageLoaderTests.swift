885d83e92d9a05aa41d794fd209750f2d2748b1f
Peter Wu <petey888@gmail.com>
2025-01-29 17:29:52 -0800

    Implements remote image loader test

---
+++ Recipes/Service/ImageLoader/Tests/ImageLoaderTests/RemoteImageLoaderTests.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteImageLoaderTests.swift
+//  ImageLoader
+//
+//  Created by Peter Wu on 1/29/25.
+//
+@testable import ImageLoader
+import Foundation
+import Testing
+
+struct RemoteImageLoaderTests {
+    let cache: NSCacheContainer
+    let loader: RemoteImageLoader
+    
+    init() {
+        self.cache = NSCacheContainer()
+        self.loader = .init(cache: cache)
+    }
+
+    @Test func remoteImageFirstLoad() async throws {
+        let urlString = "https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/small.jpg"
+        let (_, data) = try await loader.fetch(urlString)
+        let nsurl = try #require(NSURL(string: urlString))
+        let cachedData = try #require(cache.item(for: nsurl))
+        #expect(data == (cachedData as Data))
+    }
+}
