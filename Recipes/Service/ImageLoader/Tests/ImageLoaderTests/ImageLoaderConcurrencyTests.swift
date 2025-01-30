38114fea5910b47ead461432248853d7c0e1c780
Peter Wu <petey888@gmail.com>
2025-01-29 17:43:47 -0800

    Implements image loading concurrency test

---
+++ Recipes/Service/ImageLoader/Tests/ImageLoaderTests/ImageLoaderConcurrencyTests.swift
@@ -0,0 +1,42 @@
+//
+//  ImageLoaderConcurrencyTests.swift
+//  ImageLoader
+//
+//  Created by Peter Wu on 1/29/25.
+//
+
+@testable import ImageLoader
+import Foundation
+import Testing
+
+struct ImageLoaderConcurrencyTests {
+    let cache: UIIMageDictionaryContainer
+    let loader: BundleImageLoader
+    let imageFileNames: [String] = (1...100).map { "UIImage_\($0)" }
+    
+    init() {
+        cache = .init()
+        loader = .init(cache: cache)
+    }
+
+    @Test func parallelLoadingImages() async throws {
+        let imageData = try await withThrowingTaskGroup(of: (String, Data).self) { group in
+            var accumulated = [(String, Data)]()
+            for name in imageFileNames {
+                group.addTask {
+                    return try await loader.fetch(name)
+                }
+            }
+            
+            for try await data in group {
+                accumulated.append(data)
+            }
+            return accumulated
+        }
+        
+        #expect(imageData.count == 100)
+        let urls = Set(imageData.map { $0.0 })
+        #expect(urls.count == 100)
+    }
+
+}
