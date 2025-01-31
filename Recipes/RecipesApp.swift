b3fd190c498ebdad109afa693a634b36159fe69b
Peter Wu <petey888@gmail.com>
2025-01-31 11:06:45 -0800

    Extracts cache initialization to type property and avoid force app to crash when disk cache initialization fails, by defaulting to memory cache

--- Recipes/RecipesApp.swift
+++ Recipes/RecipesApp.swift
@@ -21,9 +21,20 @@ struct RecipesApp: App {
     #endif
     @State private var imageAssetViewModel = ImageAssetViewModel(
         imageService: RemoteImageLoader(
-            cache: try! DiskCache(directoryName: Bundle.main.bundleIdentifier ?? "com.peterwu.recipes")
+            cache: Self.cache
         )
     )
+    static var cache: any CacheProtocol<String, Data> = {
+        var cache: any CacheProtocol<String, Data>
+        do {
+            cache = try DiskCache(directoryName: Bundle.main.bundleIdentifier ?? "com.peterwu.recipes")
+        } catch {
+            print("Disk cache initialization failed due to \(error.localizedDescription).\nRevert to memory cache")
+            // Uses memory cache when disk cache fails
+            cache = InMemoryCache()
+        }
+        return cache
+    }()
     
     var body: some Scene {
         WindowGroup {
