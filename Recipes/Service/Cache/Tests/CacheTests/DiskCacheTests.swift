ebb071deb35533774c878fb10602e0d56819e6e2
Peter Wu <petey888@gmail.com>
2025-01-31 13:17:31 -0800

    Implements cache item test

--- Recipes/Service/Cache/Tests/CacheTests/DiskCacheTests.swift
+++ Recipes/Service/Cache/Tests/CacheTests/DiskCacheTests.swift
@@ -5,12 +5,25 @@
 //  Created by Peter Wu on 1/31/25.
 //
 
+@testable import Cache
+import Foundation
 import Testing
 
 struct DiskCacheTests {
-
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    
+    let fileManager = FileManager.default
+    let cache: DiskCache
+    
+    init() {
+        cache = try! .init(fileManager: fileManager, directoryName: "DiskCacheTest")
     }
-
+    
+    @Test func cacheItem() async throws {
+        cache.removeAll()
+        let item = "TestItem.txt"
+        let itemData = try #require(item.data(using: .utf8))
+        cache.set(itemData, for: item)
+        let data = try #require(cache.item(for: item))
+        #expect(itemData == data)
+    }
 }
