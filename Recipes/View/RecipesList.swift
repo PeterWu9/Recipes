30b8a0f7f84b5be3c85e14a2d96c839db21dbfa0
Peter Wu <petey888@gmail.com>
2025-02-18 00:05:11 -0800

    Implements sorting by dish name

--- Recipes/View/RecipesList.swift
+++ Recipes/View/RecipesList.swift
@@ -4,27 +4,104 @@
 //
 //  Created by Peter Wu on 1/27/25.
 //
+import Cache
+import ImageLoader
+import RecipesRepository
 import SwiftUI
 
 struct RecipesList: View {
-    let recipes: [RecipeCell.CellData]
+    let recipesData: [RecipeCell.CellData]
     
+    @State private var recipes: [RecipeCell.CellData]
+    @State private var nameSortSelection = SortSelection.default
+    
+    init(recipes: [RecipeCell.CellData]) {
+        self.recipesData = recipes
+        _recipes = .init(wrappedValue: recipes)
+    }
+    
+    enum SortSelection: String {
+        case alphabetical = "A-Z"
+        case alphabeticalReversed = "Z-A"
+        case `default` = "default"
+    }
+
     var body: some View {
+        let _ = Self._printChanges()
         LazyVStack(alignment: .leading) {
-            ForEach(recipes, id: \.id) {
+            ForEach(recipes) {
                 RecipeCell(data: $0)
             }
         }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Menu("Sort By") {
+                    Picker("Name", selection: $nameSortSelection) {
+                        Text(SortSelection.default.rawValue)
+                            .tag(SortSelection.default)
+                        Text(SortSelection.alphabetical.rawValue)
+                            .tag(SortSelection.alphabetical)
+                        Text(SortSelection.alphabeticalReversed.rawValue)
+                            .tag(SortSelection.alphabeticalReversed)
+                    }
+                }
+            }
+        }
+        .onChange(of: nameSortSelection, { _, newValue in
+                switch newValue {
+                case .alphabetical:
+                    recipes = recipesData.sorted(using: KeyPathComparator(\.name))
+                case .alphabeticalReversed:
+                    recipes = recipesData
+                        .sorted(using: KeyPathComparator(\.name, order: .reverse))
+                case .default:
+                    recipes = recipesData
+                }
+        })
     }
 }
 
-#Preview(
-    traits: .modifier(AllRecipesPreviewData()), .modifier(BundleImageAssetPreviewData())
-) {
-    AllRecipesPreviewContainer { recipes in
-        ScrollView {
-            RecipesList(recipes: recipes)
+struct PreviewContainer: View {
+    @State private var viewModel = ViewModel(
+        repository: InMemoryRecipesRepository(
+            configuration: .init(mode: .allRecipes)
+        )
+    )
+    @State private var imageAssetViewModel = ImageAssetViewModel.init(
+        imageService: BundleImageLoader(
+            maxLoadingTime: 1,
+            cache: InMemoryCache()
+        )
+    )
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                RecipesList(recipes: viewModel.allRecipes)
+                    .padding()
+                    .task {
+                        await viewModel.fetchAllRecipes()
+                    }
+            }
         }
+        .environment(viewModel)
+        .environment(imageAssetViewModel)
     }
-    .padding()
 }
+
+#Preview {
+    PreviewContainer()
+}
+
+// TODO: Investigate - maybe a bug in traits not able to work with @State?
+//#Preview(
+//    traits: .modifier(AllRecipesPreviewData()), .modifier(BundleImageAssetPreviewData())
+//) {
+//    AllRecipesPreviewContainer { recipes in
+//        ScrollView {
+//            RecipesList(recipes: recipes)
+//        }
+//    }
+//    .padding()
+//}
+
+extension RecipeCell.CellData: Identifiable { }
